a1d23cae644db4452b510bf0a9b6e9660cfd8b49
Madison Bonavita <bona.maddy@gmail.com>
2019-04-07 22:03:53 -0400

    Render testing 1.0

--- src/admin.test.js
+++ src/admin.test.js
@@ -1,43 +1,9 @@
-//import axios from 'axios'; 
-//import MockAdapter from 'axios-mock-adapter';
-//const admin = require('./Admin.js')
-//let url; 
-//
-//if (process.env.REACT_APP_BACKEND_HOST) { url = process.env.REACT_APP_BACKEND_HOST; }
-//    else { url = "http://localhost:5000"; }
-//
-////app.get('/api/users', async (req, res) => {
-////        const collection = db.collection("users"); 
-////        const users = await collection.find().toArray();
-////        res.send(JSON.stringify(users))
-////    });
-//
-//mock.onGet('api/users').reply(200, {
-//  users: [
-//    { id: 1, name: 'John Smith' }
-//  ]
-//});
-//
-//// POST to mock
-//// createuser() {
-////         return axios.post(proxyurl + "/api/admin/newuser", {
-////            name_first: 'Madison', 
-////            name_last: 'Nosidam',
-////            username: 'madison', 
-////            password: 'madisonpw', 
-////            tools_auth: ['MAD', 'ISO', 'N'],
-////            qi_auth: ['NOS', 'IDAM']
-////        })
-////        .then(function (response) {
-////        console.log(response);
-////        })
-////        .catch(function (error) {
-////        console.log(error);
-////        }) 
-////    }
-//
-////jest.mock('axios');
-////test('should create user', () => {
-////    const user = 
-////    
-////})
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Admin from './Admin.js';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Admin/>, div);
+});
+
