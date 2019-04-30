d8faa8c49c31eccd9d2b4ab85314cb78a8b843d2
Neil Sides <neilsides@gmail.com>
2019-04-30 11:55:05 -0400

    Update About.js

--- src/About.js
+++ src/About.js
@@ -6,10 +6,10 @@ class About extends Component {
       <div>
         <h2>About Us</h2>
         <h4>Vision</h4>
-        <p>Older adults with serious illness will live peaceful lives at home. To achieve this vision, we help older adults in nursing homes develop a home-based team for managing the patient’s serious illnesses and functional losses home. </p>
+        <p>Older adults with serious illness will live peaceful lives at home. To achieve this vision, we help older adults in nursing homes develop a home-based team for managing the patient’s serious illnesses and functional losses at home. </p>
         <h4>Team</h4>
        
-            <h6>Mark Toles Phd RN</h6>
+            <h6>Mark Toles PhD RN</h6>
             <p>Assistant Professor, UNC-Chapel Hill</p>
             <p>School of Nursing</p>
             <p>mtoles@email.unc.edu</p>
